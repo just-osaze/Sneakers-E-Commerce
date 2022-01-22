44c0090cd04920a145c1f895c056ea4c0716a0dc
just-osaze <osaigbevo.osaz@gmail.com>
2022-01-22 19:57:49 +0100

    Added feature javascript hover

--- public/main.js
+++ public/main.js
@@ -1,10 +1,63 @@
-function zoom(preview){
+function zoom1(preview){
   document.getElementById('image').src = preview.src.replace("_th", "");
+  document.getElementById('image1').style.opacity = "0.5";
+  document.getElementById('image1').style.border = "thick solid #ff7d1a";
+
+  document.getElementById('image2').style.border = "none";
+  document.getElementById('image3').style.border = "none";
+  document.getElementById('image4').style.border = "none";
+
+  document.getElementById('image2').style.opacity = "100";
+  document.getElementById('image3').style.opacity = "100";
+  document.getElementById('image4').style.opacity = "100";
+}
+function zoom2(preview){
+  document.getElementById('image').src = preview.src.replace("_th", "");
+  document.getElementById('image2').style.opacity = "0.5";
+  document.getElementById('image2').style.border = "thick solid #ff7d1a";
+
+  document.getElementById('image1').style.border = "none";
+  document.getElementById('image3').style.border = "none";
+  document.getElementById('image4').style.border = "none";
+
+  document.getElementById('image1').style.opacity = "100";
+  document.getElementById('image3').style.opacity = "100";
+  document.getElementById('image4').style.opacity = "100";
+}
+function zoom3(preview){
+  document.getElementById('image').src = preview.src.replace("_th", "");
+  document.getElementById('image3').style.opacity = "0.5";
+  document.getElementById('image3').style.border = "thick solid #ff7d1a";
+
+  document.getElementById('image1').style.border = "none";
+  document.getElementById('image2').style.border = "none";
+  document.getElementById('image4').style.border = "none";
+
+  document.getElementById('image1').style.opacity = "100";
+  document.getElementById('image2').style.opacity = "100";
+  document.getElementById('image4').style.opacity = "100";
+
+}
+function zoom4(preview){
+  document.getElementById('image').src = preview.src.replace("_th", "");
+  document.getElementById('image4').style.opacity = "0.5";
+  document.getElementById('image4').style.border = "thick solid #ff7d1a";
+
+  document.getElementById('image1').style.border = "none";
+  document.getElementById('image2').style.border = "none";
+  document.getElementById('image3').style.border = "none";
+
+  document.getElementById('image1').style.opacity = "100";
+  document.getElementById('image2').style.opacity = "100";
+  document.getElementById('image3').style.opacity = "100";
+
 }
 
-function unDo(preview){
-  document.getElementById('image').src = preview.src.replace('../images/image-product-1.jpg');
-}
+
+// function zoom(preview){
+//   document.getElementById('image').src = preview.src.replace("_th", "");
+//   document.getElementById('preview').style.opacity = "0.5";
+// }
 
 // Button counter
 count = 0
@@ -17,7 +70,6 @@ function reverseCounter() {
   }
   
   counter.innerHTML = + count;
-  console.log('yes')
 }
 
 function forwardCounter() {
@@ -25,6 +77,5 @@ function forwardCounter() {
   count += 1
 
   counter.innerHTML = + count;
-  console.log('clicked')
 }
 
